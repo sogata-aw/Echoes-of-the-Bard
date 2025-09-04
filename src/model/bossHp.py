0e86cb6a9078fdb805858602eccd216b3cdd5a98
nils-iut <nilsrayot@gmail.com>
2025-09-04 15:07:37 +0200

    debut modification barre hp

--- src/model/bossHp.py
+++ src/model/bossHp.py
@@ -13,20 +13,19 @@ class BossHp(pg.sprite.Sprite):
         self.cadre = pg.image.load(os.path.join("assets","boss","ogre","hp", "bar.png")).convert_alpha()
         self.cadre = pg.transform.scale(self.cadre, (self.width, self.height))
 
-        self.image = pg.Surface((self.width, self.height))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (self.boss.rect.x, self.boss.rect.y - 10)
+        self.rect = self.cadre.get_rect()
+        self.rect.topleft = (self.boss.rect.x, self.boss.rect.y +150)
 
         self.update()
 
     def update(self):
         # dessiner le cadre
-        self.image.blit(self.cadre, (0, 0))
+        self.cadre.blit(self.cadre, (0, 0))
         # Dessiner la barre
         fill_width = int((self.boss.hp / self.boss.max_hp) * self.width)
         # Dessiner la barre verte (hp restant)
         if fill_width > 0:
-            pg.draw.rect(self.image, (0, 255, 0), (10, 10, fill_width -50, self.height -50))
+            pg.draw.rect(self.cadre, (0, 255, 0), (10, 10, fill_width -50, self.height -50))
         # Supprimer la barre si le boss est mort
         if self.boss.hp == 0:
             self.kill()
