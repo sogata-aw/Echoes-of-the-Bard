71f5938b718409214665c96d26c1a6ca0363bd26
Sogata <maximer819@gmail.com>
2025-09-06 15:40:08 +0200

    Ajout d'input sur l'ui

--- src/model/inputUI.py
+++ src/model/inputUI.py
@@ -18,11 +18,17 @@ class InputUi(pygame.sprite.Sprite):
         note_02 = init_sprite(os.path.join(self.NOTE, "note-02.svg"), 825, 722)
         note_03 = init_sprite(os.path.join(self.NOTE, "note-03.svg"), 885, 723)
         note_04 = init_sprite(os.path.join(self.NOTE, "note-04.svg"), 940, 722)
+        bard = init_sprite(os.path.join("assets", "game", "bard", "tete-bard.png"), 600, 720, (32, int(32*298 / 258)))
+        bard.image.set_alpha(51)
 
         w_input = init_sprite(os.path.join(self.LETTERS, "W.svg"), 785, 720)
         x_input = init_sprite(os.path.join(self.LETTERS, "X.svg"), 850, 722)
         c_input = init_sprite(os.path.join(self.LETTERS, "C.svg"), 905, 723)
         v_input = init_sprite(os.path.join(self.LETTERS, "V.svg"), 965, 720)
+        space_input = init_sprite(os.path.join(self.LETTERS, "space.svg"), 640, 727, (80,26))
+
+        bard.add(self.input_group)
+        space_input.add(self.input_group)
 
         w_input.add(self.input_group)
         note_01.add(self.input_group)
